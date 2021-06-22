dafa606496593211e5d58d2551a04c4631cff3bf
Will Spencer <willspencer16@gmail.com>
2021-06-22 14:48:54 +0100

    Added list component and clear button

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
   return (
     <main>
       <section className="container">
-        <h3>0 birthdays today</h3>
-        <List />
-        <button onClick={()=> console.log('you clicked me')}>clear all</button>
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button onClick={()=> setPeople([])}>clear all</button>
       </section>
     </main>
   );
